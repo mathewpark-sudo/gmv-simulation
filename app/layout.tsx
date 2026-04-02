54f830d3944838c0868509ec870e55433b4b22fb
mathewpark-sudo <mathew.park@handys.co.kr>
2026-04-02 17:38:45 +0900

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from 'next'
+import './globals.css'
+
+export const metadata: Metadata = {
+    title: 'GMV 시뮬레이션',
+    description: 'GMV Target Simulation',
+}
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return (
+          <html lang="ko">
+                <body>{children}</body>body>
+          </html>html>
+        )
+}</html>
